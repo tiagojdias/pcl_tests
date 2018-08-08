c8fb1d16c7e09507bcb2607ae68da3440111a8d7
Ilia <ilia.prodrone@gmail.com>
2018-08-08 11:06:11 +0100

    style corrected

--- matrix_transform.cpp
+++ matrix_transform.cpp
@@ -8,8 +8,7 @@
 #include <pcl/visualization/pcl_visualizer.h>
 
 // This function displays the help
-void
-showHelp(char * program_name)
+void showHelp(char * program_name)
 {
     std::cout << std::endl;
     std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
@@ -17,12 +16,12 @@ showHelp(char * program_name)
 }
 
 // This is the main function
-int
-main (int argc, char** argv)
+int main (int argc, char** argv)
 {
     // Show help
-    if (pcl::console::find_switch (argc, argv, "-h") || pcl::console::find_switch (argc, argv, "--help")) {
-        showHelp (argv[0]);
+    if (pcl::console::find_switch(argc, argv, "-h") || pcl::console::find_switch(argc, argv, "--help"))
+    {
+        showHelp(argv[0]);
         return 0;
     }
 
@@ -30,17 +29,18 @@ main (int argc, char** argv)
     std::vector<int> filenames;
     bool file_is_pcd = false;
 
-    filenames = pcl::console::parse_file_extension_argument (argc, argv, ".ply");
+    filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");
 
-    if (filenames.size () != 1)  {
-        filenames = pcl::console::parse_file_extension_argument (argc, argv, ".pcd");
+    if (filenames.size() != 1)
+    {
+        filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
 
-        if (filenames.size () != 1) {
-            showHelp (argv[0]);
+        if(filenames.size() != 1)
+        {
+            showHelp(argv[0]);
             return -1;
-        } else {
-            file_is_pcd = true;
         }
+        else file_is_pcd = true;
     }
 
     // Load file | Works with PCD and PLY files
@@ -57,7 +57,7 @@ main (int argc, char** argv)
     }
     else
     {
-        if (pcl::io::loadPLYFile(argv[filenames[0]], *source_cloud) < 0)
+        if(pcl::io::loadPLYFile(argv[filenames[0]], *source_cloud) < 0)
         {
             std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
             showHelp(argv[0]);
@@ -78,20 +78,19 @@ main (int argc, char** argv)
     */
     Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();
 
-
     // Define a rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
     float theta = M_PI/4; // The angle of rotation in radians
-    transform_1 (0,0) = cos (theta);
-    transform_1 (0,1) = -sin(theta);
-    transform_1 (1,0) = sin (theta);
-    transform_1 (1,1) = cos (theta);
-    //    (row, column)
+    transform_1(0,0) = cos(theta);
+    transform_1(0,1) = -sin(theta);
+    transform_1(1,0) = sin(theta);
+    transform_1(1,1) = cos(theta);
+    //   (row, column)
 
     // Define a translation of 2.5 meters on the x axis.
-    transform_1 (0,3) = 2.5;
+    transform_1(0,3) = 2.5;
 
     // Print the transformation
-    printf ("Method #1: using a Matrix4f\n");
+    printf("Method #1: using a Matrix4f\n");
     std::cout << transform_1 << std::endl;
 
     /*  METHOD #2: Using a Affine3f
@@ -103,38 +102,39 @@ main (int argc, char** argv)
     transform_2.translation() << 2.5, 0.0, 0.0;
 
     // The same rotation matrix as before; theta radians around Z axis
-    transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
+    transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
 
     // Print the transformation
-    printf ("\nMethod #2: using an Affine3f\n");
+    printf("\nMethod #2: using an Affine3f\n");
     std::cout << transform_2.matrix() << std::endl;
 
     // Executing the transformation
-    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
     // You can either apply transform_1 or transform_2; they are the same
-    pcl::transformPointCloud (*source_cloud, *transformed_cloud, transform_2);
+    pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2);
 
     // Visualization
     printf(  "\nPoint cloud colors :  white  = original point cloud\n"
              "                        red  = transformed point cloud\n");
-    pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+    pcl::visualization::PCLVisualizer viewer("Matrix transformation example");
 
     // Define R,G,B colors for the point cloud
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (source_cloud, 255, 255, 255);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler(source_cloud, 255, 255, 255);
     // We add the point cloud to the viewer and pass the color handler
-    viewer.addPointCloud (source_cloud, source_cloud_color_handler, "original_cloud");
+    viewer.addPointCloud(source_cloud, source_cloud_color_handler, "original_cloud");
 
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler (transformed_cloud, 230, 20, 20); // Red
-    viewer.addPointCloud (transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler(transformed_cloud, 230, 20, 20); // Red
+    viewer.addPointCloud(transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
 
-    viewer.addCoordinateSystem (1.0, "cloud", 0);
+    viewer.addCoordinateSystem(1.0, "cloud", 0);
     viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
-    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
-    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
+    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
     //viewer.setPosition(800, 400); // Setting visualiser window position
 
-    while (!viewer.wasStopped ()) { // Display the visualiser until 'q' key is pressed
-        viewer.spinOnce ();
+    while (!viewer.wasStopped())
+    { // Display the visualiser until 'q' key is pressed
+        viewer.spinOnce();
     }
 
     return 0;
