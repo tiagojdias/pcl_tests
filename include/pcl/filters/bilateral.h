521a89e1cfb6c7a287cba01ff13581885e184cd5
Ilia <ilia.prodrone@gmail.com>
2018-09-06 12:03:05 +0100

    bilateral folder structure added

---
+++ include/pcl/filters/bilateral.h
@@ -0,0 +1,16 @@
+#ifndef PCL_FILTERS_BILATERAL_H_
+#define PCL_FILTERS_BILATERAL_H_
+
+#include <pcl/filters/filter.h>
+
+namespace pcl
+{
+
+template<typename PointT>
+class BilateralFilter : public Filter<PointT>
+{
+};
+
+}
+
+#endif // PCL_FILTERS_BILATERAL_H_
---
+++ include/pcl/filters/impl/bilateral.hpp
@@ -0,0 +1,6 @@
+#ifndef PCL_FILTERS_BILATERAL_IMPL_H_
+#define PCL_FILTERS_BILATERAL_IMPL_H_
+
+#include <pcl/filters/bilateral.h>
+
+#endif // PCL_FILTERS_BILATERAL_H_
